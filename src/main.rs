d3d6ceb1129e78c9cf34970eb2457103026662b8
Jessica Wang <wangjessica02@gmail.com>
2025-10-30 15:15:55 -0400

    Added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,49 @@
 use std::env;
+use std::fs;
+use std::process;
 
 fn main() {
+    // Collect command line arguments
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    println!("Searching for {query}");
-    println!("In file {file_path}");
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 
     dbg!(args);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+// Constructor
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config {query, file_path}
 }
